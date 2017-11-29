af2128714609cdf6a530179e88ff2c3210ec7f7b
Cynthia Deng <cynthiad215@gmail.com>
2017-11-28 23:54:38 -0800

    ADD Working version of input

---
+++ haskinput.hs
@@ -0,0 +1,17 @@
+--CPSC 312
+
+rout :: Int -> Int -> Double
+rout x y =
+    if  x < y 
+    then 0.1
+        else 0.3
+
+
+main = do
+       putStrLn "enter value for x: "
+       input1 <- getLine
+       putStrLn "enter value for y: " 
+       input2 <- getLine 
+       let x = (read input1 :: Int)
+       let y = (read input2 :: Int)
+       print (rout x y)
