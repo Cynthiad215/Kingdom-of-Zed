9acf4438bce5426c50e72f2070b72a9f9a78d2ee
kotes95 <nick.chim95@gmail.com>
2017-11-30 00:22:59 -0800

    Rename functions, add validate_solution

--- solver.hs
+++ solver.hs
@@ -4,31 +4,53 @@ import Data.List
 generate n = permutations [1..n]
 
 -- Solve game board
+-- TO DO
 solve (top, right, bottom, left) = top
 
 -- Return true if the given board is a valid solution given input
-is_valid_solution (top, right, bottom, left) input = True
+  validate_merchants top (top_paths input) &&
+  validate_merchants right (right_paths input) &&
+  validate_merchants bottom (bottom_paths input) &&
+  validate_merchants left (left_paths input)
+  where
+    validate_merchants merchants paths
+      | merchants == [] || paths == [] = True
+      | otherwise = validate_posts (head merchants) (head paths) 0 && validate_merchants (tail merchants) (tail paths)
+
+-- TEST CASES
+-- is_valid_solution ([1,3,2,2],[3,2,1,2],[2,2,1,3],[2,2,3,1]) [[4,1,3,2],[2,3,4,1],[3,2,1,4],[1,4,2,3]]
+-- should return true
+-- is_valid_solution ([1,3,2,2],[3,2,1,2],[2,2,1,3],[2,2,3,1]) [[4,1,3,2],[2,3,4,1],[4,2,1,3],[1,4,2,3]]
+-- should return false
+-- is_valid_solution ([1,3,2,2],[3,2,1,2],[2,2,1,3],[2,2,3,1]) [[1,2,3,4],[2,1,4,3],[3,4,1,2],[4,3,2,1]]
+-- should return false
 
 -- Return true if the line of posts is valid 
-is_valid_line num_posts lst llv
+validate_posts :: (Eq t, Num t, Ord a) => t -> [a] -> a -> Bool
+validate_posts num_posts lst llv
   | num_posts == 0 = True
   | lst == [] = False
-  | (head lst) > llv = is_valid_line (num_posts-1) (tail lst) (head lst)
-  | otherwise = is_valid_line num_posts (tail lst) llv
+  | head lst > llv = validate_posts (num_posts-1) (tail lst) (head lst)
+  | otherwise = validate_posts num_posts (tail lst) llv
 
 -- Returns array containing line of trading posts corresponding to merchants on top
-filter_top_posts input = get_grid_columns (length input) (length input) input
+top_paths :: [[Int]] -> [[Int]]
+top_paths rows = get_grid_columns (length rows) (length rows) rows
 
 -- Returns array containing line of trading posts corresponding to merchants to the right
-filter_right_posts input = map reverse input
+right_paths :: [[Int]] -> [[Int]]
+right_paths rows = map reverse rows
 
 -- Returns array containing line of trading posts corresponding to merchants at bottom
-filter_bottom_posts input = map reverse (reverse (get_grid_columns (length input) (length input) input))
+bottom_paths :: [[Int]] -> [[Int]]
+bottom_paths rows = map reverse $ reverse $ get_grid_columns (length rows) (length rows) rows
 
 -- Returns array containing line of trading posts corresponding to merchants to the left
-filter_left_posts input = reverse input
+left_paths :: [[Int]] -> [[Int]]
+left_paths rows = reverse rows
 
 -- Given length of rows, iteration index and input (grid as array of rows), return grid as array of columns
+get_grid_columns :: Int-> Int -> [[Int]] -> [[Int]]
 get_grid_columns n index rows 
   | index > 0 = [x!!(n-index) | x <- rows] : get_grid_columns n (index-1) rows
   | otherwise = []
