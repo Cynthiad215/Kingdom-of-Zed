c8f4751e16e48cfbba28c6c96e4d91063662292d
Cynthia Deng <cynthiad215@gmail.com>
2017-12-01 17:09:01 -0800

    ADD input checks

--- haskinput.hs
+++ haskinput.hs
@@ -6,12 +6,37 @@ rout x y =
     then 0.1
         else 0.3
 
+--main = do
+--       putStrLn "enter the dimension for your grid: "
+--       input1 <- getLine
+--       putStrLn "enter values for the top index, separated by a comma: " 
+--       input2 <- getLine 
+--       let dim = (read input1 :: Int)
+--       let top = (read input2 :: [Int])
+--       if (length top == dim)
+--        then print ("true")
+--        else print ("false")
+--       print ("done!")
 
 main = do
-       putStrLn "enter value for x: "
-       input1 <- getLine
-       putStrLn "enter value for y: " 
-       input2 <- getLine 
-       let x = (read input1 :: [Int])
-       let y = (read input2 :: [Int])
-       print (x, y)
+       putStrLn "enter the dimension for your grid: "
+       dimension <- getLine
+       let dim = (read dimension :: Int)
+       if (dim > 4 || dim == 0)
+        then do 
+         print ("Timeout error, please try again")
+         main
+        else do 
+         topfn dim
+
+
+topfn dim = do
+            putStrLn "enter values for the top index: " 
+            top1 <- getLine 
+            let t1 = (read top1 :: Int)
+            if (elem t1 [0..dim])
+             then print ("true")
+             else do 
+             print ("oops that's not valid!") 
+             topfn dim
+            print ("done!")
